b13d6e9411f9c8a3dcf3c3eb3500af83c1a94b90
kaushalkumar98 <kaushalkumar3795@gmail.com>
2024-01-22 11:03:47 +0530

    Routers => {Making /admin & /shop routes}

---
+++ expTask-3.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express()
+
+const adminRoutes = require('./routes/admin.js');
+const shopRoutes = require('./routes/shop.js');
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/admin', adminRoutes);
+app.use('/shop', shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page Not Found</h1>');
+})
+
+app.listen(3000);
